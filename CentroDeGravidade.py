e9c699a9432917f7ab781166a19282312b286a12
MateusKen <54600427+MateusKen@users.noreply.github.com>
2022-10-22 01:37:53 -0300

    Update CentroDeGravidade.py

--- CentroDeGravidade.py
+++ CentroDeGravidade.py
@@ -25,30 +25,66 @@ def imprimeMatriz(M): #isso da certo
             print(' ', M[i][j],' ', end='')
         print('\n')
 
-def verificaCentro(M): #isso ainda não da certo (apenas para linhas)
-    indice = 1
-    menor = 0
+def verificaCentro(M): #isso da certo(apenas para linhas)
+    indiceL = 1
+    indiceC = 1
+    menorL = 0
+    menorC = 0
+    contL = 0
+    contC = 0
     for i in range(1,len(M)-1): #i que percorre cada linha, menos a primeira e a última
-        soma = []
-        for j in range(1,len(M)-1):
+        somaL = []
+        for j in range(1,len(M)-1):# para somar linhas que não são do indice
             if i != j:
-                print(i,j)
-                soma += M[j] #soma em uma lista todos os elementos menos o que estiver com o índice
-            print(soma)
-        soma = sum(soma)
+                
+                somaL += M[j] #soma em uma lista todos os elementos menos o que estiver com o índice
+        
+        somaL = sum(somaL)
         
         if i == 1: #para 1ª comparação
-            menor = soma
-        elif soma < menor: 
-            menor = soma
-            indice = i+1
+            menorL = somaL
+        elif somaL < menorL: 
+            menorL = somaL
+            indiceL = i+1
+        elif somaL == menorL:
+            contL += 1
+            if contL == len(M)-3:
+                indiceL = 'X'
+
+    for i in range(1, len(M[0])): #i que percorre cada coluna, menos a primeira e a última
+        somaC = []
+        for j in range(1, len(M[0])):
+            coluna = [0]*len(M)
+
+            for k in range(len(M)): #faz uma lista com a coluna
+                coluna[k] = M[k][i]
+
+            if i != j:
+
+                somaC = sum(coluna)
             
-    return indice
+            if i == 1:
+                menorC = somaC
+            elif somaC < menorC:
+                menorC = somaC
+                indiceC = i+1
+
+            elif somaC == menorC:
+                contC += 1
+                if contC == len(M)-3:
+                    indiceC = 'X'
+                
+            
+                
+                
+                
+    return indiceL, indiceC
 
 
 def main():
     arquivo = leArquivo('texto.txt')
     imprimeMatriz(arquivo)
-    print('Centro: ', '(', verificaCentro(arquivo),')')
+
+    print('Centro: ', '(', verificaCentro(arquivo)[0],',',verificaCentro(arquivo)[1],')')
     
 main()
